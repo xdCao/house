250e44f01905538ffd829cbd162ed8c24895a3e2
caohao <705083979@qq.com>
2019-04-30 15:57:39 +0800

    es状态监控

---
+++ src/main/java/com/xdcao/house/base/task/EsMonitor.java
@@ -0,0 +1,56 @@
+package com.xdcao.house.base.task;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static org.apache.http.HttpStatus.SC_OK;
+
+/**
+ * @Author: buku.ch
+ * @Date: 2019-04-30 15:43
+ */
+
+@Component
+public class EsMonitor {
+
+    private static final String HEAL_CHECK_API = "http://localhost:9200/_cluster/health";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EsMonitor.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Scheduled(fixedDelay = 5000)
+    public void healthCheck() {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet get = new HttpGet(HEAL_CHECK_API);
+        try {
+            CloseableHttpResponse httpResponse = httpClient.execute(get);
+            if (httpResponse.getStatusLine().getStatusCode() != SC_OK) {
+                LOGGER.error("Can not access ES service! Please check");
+            } else {
+                String body = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
+                JsonNode jsonNode = objectMapper.readTree(body);
+                String status = jsonNode.get("status").asText();
+                LOGGER.info("elasticSearch status: {}",status);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
